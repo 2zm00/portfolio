cf06fd4df7c76471e6f195ba4895a9e089593e51
2zm00 <ljm010301@naver.com>
2025-03-04 17:30:37 +0900

    chore: 오타 수정

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -5,7 +5,7 @@ export default function TrainingSection() {
 	return (
 		<>
 			<Card title="교육" delay={500}>
-				멋쟁이 사자처럼 Banckend Python 12기. <br/>
+				멋쟁이 사자처럼 Backend Python 12기. <br/>
 				- 파이썬을 활용한 고도화 아키텍처 설계 및 개발 <br/>
 				- 웹 개발 및 AI 기반 API 개발 <br/>
 				- 팀 프로젝트를 통한 실무 협업 경험
